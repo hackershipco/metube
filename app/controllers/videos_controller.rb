0f1eb1040ecddf5931c960c0e326cc13ee25374d
Dvid Silva <dsilva@doctorondemand.com>
2014-10-26 16:26:59 -0700

    added new and create actions to controller, created view

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,4 +1,19 @@
 class VideosController < ApplicationController
   def index
   end
+
+  def new
+    @video = Video.new
+  end
+
+  def create
+    @video = Video.new(params[:video])
+    if @video.save
+      flash[:success] = 'Video added!'
+      redirect_to root_url
+    else
+      render 'new'
+    end
+  end
+
 end
