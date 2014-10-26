8f3c5abbf34fd586dfeb69f704778cf0e348a0b4
Dvid Silva <dsilva@doctorondemand.com>
2014-10-26 16:33:23 -0700

    fixed the model with video_params, created shared/errors

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,4 +1,10 @@
 class Video < ActiveRecord::Base
-  attr_accessible :link
-  params.require(:video).permit(:link)
+  # attr_accessible :link
+
+  private
+
+    def video_params
+      params.require(:video).permit(:link)
+    end
+
 end
